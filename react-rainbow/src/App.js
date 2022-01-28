cc1860872b79a7b9000667ac9e2ea66cde31d5ab
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-01-28 16:02:06 -0500

    first commit

---
+++ react-rainbow/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock';
+
+function App() {
+  let colors = ["violet", "blue", "lightblue", "green", "green", "greenyellow", "yellow", "orange", "red"]
+  
+  let colorMap = colors.map((color, i) => {
+    return (
+    <ColorBlock color ={color} />
+    )
+  })
+  
+  return (
+    <div className="App">
+      {colorMap}
+    </div>
+  );
+}
+
+export default App;

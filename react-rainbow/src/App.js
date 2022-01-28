2b0f9dc2b3f67caeb6f1a24a99c74a73fae07957
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-01-28 16:56:02 -0500

    finsihed project, errors done

--- react-rainbow/src/App.js
+++ react-rainbow/src/App.js
@@ -1,20 +1,37 @@
-import React, { useState } from 'react'
+import "./App.css";
+
+import React, { useState } from 'react';
+// Make sure to import the component we just built:
 import ColorBlock from './ColorBlock';
+import ColorForm from "./ColorForm";
 
-function App() {
-  let colors = ["violet", "blue", "lightblue", "green", "green", "greenyellow", "yellow", "orange", "red"]
-  
-  let colorMap = colors.map((color, i) => {
+function App(){
+    let [colors, setColors] = useState([
+        'violet', 
+        'blue',
+        'lightblue', 
+        'green',
+        'greenyellow', 
+        'yellow',
+        'orange', 
+        'red'
+    ]);
+    
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color={color} />
+        )
+    });
+
+    const addColor = (newColor) => {
+      setColors([...colors, newColor]);
+    };
+
     return (
-    <ColorBlock color={color} />
-    )
-  })
-  
-  return (
-    <div className="App">
-      {colorMap}
-    </div>
-  );
-}
-
+      <div className="App">
+        {colorMap}
+        <ColorForm addColor={addColor} />
+      </div>
+    );
+  }
 export default App;
